3fbd49c95dc6fccac209ee78729244df12192aca
Greg Venech <greg.venech@gmail.com>
2017-07-19 18:39:24 -0400

    refactor(caching): remove extraneous comment

--- examples/caching/src/index.js
+++ examples/caching/src/index.js
@@ -4,7 +4,6 @@ import Print from './print';
 function component() {
   var element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.onclick = Print.bind(null, 'Hello webpack!');
